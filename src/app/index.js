fbd4f7faf0c80364125405a3dd1d7b612901a5ca
Artur Catunda <arturmcatunda@gmail.com>
2024-08-24 15:56:29 -0300

    added expo-router

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
-export default function App() {
+export default function index() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-500">
-      <Text>Open up App.js to start working on your bruh!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
